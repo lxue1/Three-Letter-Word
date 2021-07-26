117ea66e5ad4e9c70f9a0f5156d9acd688ab584c
Luke Xue <lukexue064@gmail.com>
2021-07-26 12:33:46 -0500

    Initial Commit

---
+++ Three Letter Word/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Three Letter Word
+//
+//  Created by Luke Xue on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Luke Xue on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
